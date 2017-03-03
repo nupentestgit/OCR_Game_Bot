83ac22a8b6bdf8723e85a102b4be6ee24e94a8a2
nupentestgit <nupentestgit@users.noreply.github.com>
2017-03-03 14:49:04 +0100

    Backup before format

--- src/screenshots/Screener.java
+++ src/screenshots/Screener.java
@@ -4,4 +4,8 @@ import java.util.Observable;
 
 public class Screener extends Observable {
 
+	public Screener() {
+		
+	}
+	
 }
--- src/screenshots/types/DummyImage.java
+++ src/screenshots/types/DummyImage.java
@@ -1,6 +1,21 @@
 package screenshots.types;
 
+import java.awt.image.BufferedImage;
 
 public class DummyImage {
+	
+	private BufferedImage dummy;
+	
+	public DummyImage(BufferedImage img) {
+		this.dummy = img;
+	}
 
+	public BufferedImage getDummy() {
+		return dummy;
+	}
+
+	public void setDummy(BufferedImage dummy) {
+		this.dummy = dummy;
+	}
+	
 }
